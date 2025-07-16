4597f24f6aade5749bd70d37a693009442c9e16f
AlexyCatelle <alexy.catelle@oclock.school>
2025-07-16 11:53:40 +0200

    TravelLog Service

--- src/main/java/Exo1/service/TravelLogService.java
+++ src/main/java/Exo1/service/TravelLogService.java
@@ -1,4 +1,35 @@
 package Exo1.service;
 
+import Exo1.DAO.TravelLogDAO;
+import Exo1.entity.TravelLog;
+
+import java.util.List;
+
 public class TravelLogService {
+    private TravelLogDAO travelLogDAO;
+
+    public TravelLogService(TravelLogDAO travelLogDAO) {
+        this.travelLogDAO = travelLogDAO;
+    }
+
+    public void createTravelLog(TravelLog travelLog) {
+        travelLogDAO.save(travelLog);
+    }
+
+    public TravelLog getTravelLog(Long id) {
+        return travelLogDAO.findById(id);
+    }
+
+    public List<TravelLog> listTravelLogs() {
+       return travelLogDAO.findAll();
+    }
+
+    public void updateTravelLog(TravelLog travelLog) {
+        travelLogDAO.save(travelLog);
+    }
+
+    public void deleteTravelLog(Long id) {
+        travelLogDAO.delete(id);
+    }
+
 }
